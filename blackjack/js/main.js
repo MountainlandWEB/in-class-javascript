2da54c9b70b1a55a921bbc960f6cff65a0a1f5f6
Mario Garcia <mariogarcia@Marios-MacBook-Pro.local>
2020-11-12 08:56:40 -0700

    es6 modules

--- blackjack/js/main.js
+++ blackjack/js/main.js
@@ -1,17 +1,10 @@
+import { getNewDeck, getCards } from './api.js';
+import { printCards } from './dom.js';
 const player1 = 'player1';
 const player2 = 'player2';
 
 // call to get a new shuffled deck
-let deckPromise = fetch(
-  'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-).then((response) => response.json());
-
-async function getCards(deckId, cardCount) {
-  let response = await fetch(
-    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${cardCount}`
-  );
-  return response.json();
-}
+let deckPromise = getNewDeck();
 
 deckPromise.then(async (deck) => {
   let data1 = await getCards(deck.deck_id, 2);
@@ -20,19 +13,10 @@ deckPromise.then(async (deck) => {
   printCards(data2.cards, player2);
 });
 
-function printCards(cards, playerId) {
-  cards.forEach((card) => {
-    let cardHtml = `
-              <div>
-                  <img src="${card.image}">
-              </div>
-          `;
-    document.getElementById(`${playerId}-cards`).innerHTML += cardHtml;
-  });
-}
-
 async function hitMe(playerId) {
   let deck = await deckPromise;
   let data = await getCards(deck.deck_id, 1);
   printCards(data.cards, playerId);
 }
+
+window.hitMe = hitMe;
